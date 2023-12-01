4edb6d47bcc203f44d31bd7cf09992318712051e
Carson Radtke <nosrac925@gmail.com>
2023-12-01 07:48:09 -0500

    configure clang-format

--- src/01.cpp
+++ src/01.cpp
@@ -5,95 +5,93 @@
 #include <iostream>
 #include <string>
 
-[[noreturn]] void s_exit_error(const std::string & error)
+[[noreturn]] void s_exit_error( const std::string &error )
 {
   std::cerr << error << std::endl;
-  exit(EXIT_FAILURE);
+  exit( EXIT_FAILURE );
 }
 
 static std::vector<std::string> s_english_numbers = {
-  "zero",
-  "one",
-  "two",
-  "three",
-  "four",
-  "five",
-  "six",
-  "seven",
-  "eight",
-  "nine"
-};
+    "zero", "one", "two",   "three", "four",
+    "five", "six", "seven", "eight", "nine" };
 
-int8_t s_english_number(const char * ptr)
+int8_t s_english_number( const char *ptr )
 {
   int8_t rv = 0;
-  for (auto word : s_english_numbers)
+  for ( auto word : s_english_numbers )
   {
-    if (std::strncmp(word.data(), ptr, word.length()) == 0) return rv;
+    if ( std::strncmp( word.data(), ptr, word.length() ) == 0 )
+      return rv;
     rv++;
   }
   return -1;
 }
 
-int8_t s_get_last_digit(const std::string & line)
+int8_t s_get_last_digit( const std::string &line )
 {
-  const char * data = line.data();
-  const char * ptr = data + line.size() - 1;
-  while (ptr >= data)
+  const char *data = line.data();
+  const char *ptr = data + line.size() - 1;
+  while ( ptr >= data )
   {
-    if (std::isdigit(*ptr)) return *ptr - '0';
-    int8_t english_number = s_english_number(ptr);
-    if (english_number >= 0) return english_number;
+    if ( std::isdigit( *ptr ) )
+      return *ptr - '0';
+    int8_t english_number = s_english_number( ptr );
+    if ( english_number >= 0 )
+      return english_number;
     ptr--;
   }
   return -1;
 }
 
-int8_t s_get_first_digit(const std::string & line)
+int8_t s_get_first_digit( const std::string &line )
 {
-  const char * data = line.data();
-  const char * ptr = data;
-  while (*ptr)
+  const char *data = line.data();
+  const char *ptr = data;
+  while ( *ptr )
   {
-    if (std::isdigit(*ptr)) return *ptr - '0';
-    int8_t english_number = s_english_number(ptr);
-    if (english_number >= 0) return english_number;
+    if ( std::isdigit( *ptr ) )
+      return *ptr - '0';
+    int8_t english_number = s_english_number( ptr );
+    if ( english_number >= 0 )
+      return english_number;
     ptr++;
   }
   return -1;
 }
 
-static std::int64_t s_calibration_value(const std::string & line)
+static std::int64_t s_calibration_value( const std::string &line )
 {
-  int8_t first = s_get_first_digit(line);
-  int8_t last = s_get_last_digit(line);
-  if (first == '\0' || last == '\0') 
+  int8_t first = s_get_first_digit( line );
+  int8_t last = s_get_last_digit( line );
+  if ( first == '\0' || last == '\0' )
   {
-    s_exit_error("did not find any digits");
+    s_exit_error( "did not find any digits" );
   }
-  
+
   return 10 * first + last;
 }
 
-static int s_trebuchet(const std::string & input_file)
+static int s_trebuchet( const std::string &input_file )
 {
-  auto fs = std::ifstream(input_file);
-  if (!fs.is_open()) s_exit_error("could not open input file");
+  auto fs = std::ifstream( input_file );
+  if ( !fs.is_open() )
+    s_exit_error( "could not open input file" );
 
   std::int64_t calibration_value = 0;
-  while (!fs.eof())
+  while ( !fs.eof() )
   {
     std::string line;
     fs >> line;
-    calibration_value += s_calibration_value(line);
+    calibration_value += s_calibration_value( line );
   }
   std::cout << calibration_value << std::endl;
 
   return EXIT_SUCCESS;
 }
 
-int main(int argc, const char * argv[])
+int main( int argc, const char *argv[] )
 {
-  if (argc != 2) s_exit_error("missing input file");
-  return s_trebuchet(argv[1]);
+  if ( argc != 2 )
+    s_exit_error( "missing input file" );
+  return s_trebuchet( argv[1] );
 }
