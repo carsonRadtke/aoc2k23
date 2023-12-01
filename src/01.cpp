57fb2712c102179b5989e30e58cccf6cb845c9f4
Carson Radtke <nosrac925@gmail.com>
2023-12-01 07:39:35 -0500

    complete day 1

--- src/01.cpp
+++ src/01.cpp
@@ -11,27 +11,68 @@
   exit(EXIT_FAILURE);
 }
 
+static std::vector<std::string> s_english_numbers = {
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine"
+};
+
+int8_t s_english_number(const char * ptr)
+{
+  int8_t rv = 0;
+  for (auto word : s_english_numbers)
+  {
+    if (std::strncmp(word.data(), ptr, word.length()) == 0) return rv;
+    rv++;
+  }
+  return -1;
+}
+
+int8_t s_get_last_digit(const std::string & line)
+{
+  const char * data = line.data();
+  const char * ptr = data + line.size() - 1;
+  while (ptr >= data)
+  {
+    if (std::isdigit(*ptr)) return *ptr - '0';
+    int8_t english_number = s_english_number(ptr);
+    if (english_number >= 0) return english_number;
+    ptr--;
+  }
+  return -1;
+}
+
+int8_t s_get_first_digit(const std::string & line)
+{
+  const char * data = line.data();
+  const char * ptr = data;
+  while (*ptr)
+  {
+    if (std::isdigit(*ptr)) return *ptr - '0';
+    int8_t english_number = s_english_number(ptr);
+    if (english_number >= 0) return english_number;
+    ptr++;
+  }
+  return -1;
+}
+
 static std::int64_t s_calibration_value(const std::string & line)
 {
-  char first = '\0';
-  char last = '\0';
-  for (auto c : line)
-  {
-    if (std::isdigit(c))
-    {
-      if (first == '\0')
-      {
-        first = c;
-      }
-      last = c;
-    }
-  }
+  int8_t first = s_get_first_digit(line);
+  int8_t last = s_get_last_digit(line);
   if (first == '\0' || last == '\0') 
   {
     s_exit_error("did not find any digits");
   }
   
-  return 10 * (first - '0') + (last - '0');
+  return 10 * first + last;
 }
 
 static int s_trebuchet(const std::string & input_file)
